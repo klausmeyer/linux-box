a4785a4d6364a643bfe08dd442573698473acdb0
Klaus Meyer <spam@klaus-meyer.net>
2021-05-22 10:39:12 +0200

    padding in xfce terminal

--- update.sh
+++ update.sh
@@ -8,6 +8,7 @@ files=(
   ~/.Xmodmap
   ~/.config/compton.conf
   ~/.config/gtk-3.0/settings.ini
+  ~/.config/gtk-3.0/gtk.css
   ~/.config/i3/config
   ~/.config/i3blocks/config
   ~/.config/ranger/rc.conf
